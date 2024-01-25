4d3389edfc2fc1c2151495ab9d6a324b6c1fb35c
Guilherme Batista de Souza <146041046+guilhermebatista7@users.noreply.github.com>
2024-01-24 22:17:24 -0300

    Laço de repetição Js

---
+++ estudo-próprio/15-Laço-repetição/js/index.js
@@ -0,0 +1,18 @@
+/*for (let i = 0; i <= 100; i++) {
+    document.getElementById('teste').innerHTML += i + ', ';
+}*/
+
+
+var ano = new Date().getFullYear()
+
+for (let i = ano; i >= 2000; ano--) {
+    document.getElementById('iano').innerHTML += "<option value='"+ano+"'>"+ano+"</option>"
+}
+
+
+const carros = ['Gol', 'Fusca', 'Brasília', 'Del Rey', 'Chevette'];
+var tamanho = carros.length;
+
+for(let i=0; i <tamanho; i++) {
+    document.getElementById('teste').innerHTML +=carros[i] + ' - ';
+}
