7690f6f351f5618cb73d88cdf11df98fd9910780
Guilherme Batista de Souza <146041046+guilhermebatista7@users.noreply.github.com>
2024-04-04 16:34:08 -0300

    primitive data types

---
+++ estudy-roadmap/003dataTypes/01primitiveTypes/index.js
@@ -0,0 +1,15 @@
+let nome = "Guilherme"; //string
+let numero = 10; //number
+let x = 88661212121222222748n; //BigInt
+x == nome //boolean
+console.log(x == nome);
+
+let y; //undefined
+console.log(y)
+
+let z = null; //null
+console.log(z)
+
+let symbol1 = Symbol("symbol1"); //Symbol
+let symbol2 = Symbol("symbol2");
+console.log(symbol1 === symbol2);
