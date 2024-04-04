3592db36bbe1a9760ae9a42257861a5b52bc1bcc
Guilherme Batista de Souza <146041046+guilhermebatista7@users.noreply.github.com>
2024-04-03 22:29:07 -0300

    Variables rules

---
+++ estudy-roadmap/002allAboutVariables/03variablesRules/index1.js
@@ -0,0 +1,16 @@
+//Pode conter letras, números, underline e $
+//deve começar com letras, underline ou $
+
+//Existem palavras reservadas
+//Não podem começar com números
+
+//case sensitive
+
+//convenção camelCase - snake_case(underscore)
+
+let name = 1;
+let firstName = 2;
+let _full1Name = 3;
+let last_name$ = 4;
+
+console.log(name, firstName, _full1Name, last_name$);
