885ed67c661f571b0d212ea6c55c7eebc9235099
Guilherme Batista de Souza <146041046+guilhermebatista7@users.noreply.github.com>
2024-04-13 13:12:03 -0300

    Arrays

---
+++ estudy-roadmap/003dataTypes/02objectTypes/02array/index.js
@@ -0,0 +1,16 @@
+var arr = [5, "Olá", true, {teste: 1, teste: 2}];
+console.log(arr);
+
+var arr2 = [2,3,4,5];
+arr2[2] = 9;
+console.log(arr2[1]);
+
+
+var nums = [1, 2, -3, 5, 4];
+console.log(nums.length);
+console.log(nums.sort())
+
+const cars = new Array("Fusca", "Bmw", "Celta");
+cars.push("Corsa");
+console.log(cars.toString());
+console.log(cars[cars.length-1]);
