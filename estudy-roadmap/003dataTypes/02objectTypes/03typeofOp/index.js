d8e18ea24cc6c97dfd231b2b40e5bf00707977a0
Guilherme Batista de Souza <146041046+guilhermebatista7@users.noreply.github.com>
2024-04-16 09:27:43 -0300

    Typeof operator and built in objects

---
+++ estudy-roadmap/003dataTypes/02objectTypes/03typeofOp/index.js
@@ -0,0 +1,9 @@
+var car = {
+    modelo: 'civic',
+    marca: 'honda',
+    ano: '2018'
+}
+
+console.log(typeof car);
+console.log(typeof "ola");
+console.log(typeof 45);
---
+++ estudy-roadmap/003dataTypes/02objectTypes/04builtInObjects/index.js
@@ -0,0 +1,4 @@
+var date = new Date();
+console.log(date.getDate());
+console.log(date.getHours());
+console.log(date.toLocaleDateString())
