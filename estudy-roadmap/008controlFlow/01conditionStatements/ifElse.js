250527b2ee269acd821ff1bab1332e8eb0da0b8f
Guilherme Batista de Souza <146041046+guilhermebatista7@users.noreply.github.com>
2024-04-30 09:13:26 -0300

    Conditions

---
+++ estudy-roadmap/008controlFlow/01conditionStatements/ifElse.js
@@ -0,0 +1,11 @@
+let a = 'a';
+
+if(a < 10) {
+    console.log('a é menor que 10')
+} else if (a > 10) {
+    console.log('a é maior que dez')
+} else if (isNaN(a)) {
+    console.log('a não é um número')
+} else if(a == 10) {
+    console.log('a é igual a 10')
+}
---
+++ estudy-roadmap/008controlFlow/01conditionStatements/swicth.js
@@ -0,0 +1,19 @@
+let b = 3;
+
+switch(b){
+    case 1: {
+        console.log('valor 1');
+        break;
+    }
+    case 2: {
+        console.log('valor 2');
+        break;
+    }
+    case 3: {
+        console.log('valor 3');
+        break;
+    }
+    default: {
+        console.log('valor inválido')
+    }
+}
