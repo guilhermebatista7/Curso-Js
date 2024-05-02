3545b9e533513fa9e81fc12130c58bca48ff2b6f
Guilherme Batista de Souza <146041046+guilhermebatista7@users.noreply.github.com>
2024-05-02 16:26:01 -0300

    Assignment Operators

---
+++ estudy-roadmap/009Operators/01assignmentOperators/index.js
@@ -0,0 +1,12 @@
+let x, y;
+x = 5; 
+y = 2
+
+//console.log(x+=y);
+//console.log(x-=y);
+//console.log(x*=y);
+//console.log(x**=y);
+//console.log(x%=y);
+console.log(x/=y);
+
+//foi necessário fazer isso, pois um console interfere no valor de outro
