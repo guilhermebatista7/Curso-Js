85abe9a5c97299f48ee42f323aa52fbecbc2be3a
Guilherme Batista de Souza <146041046+guilhermebatista7@users.noreply.github.com>
2024-05-08 14:33:48 -0300

    Function Parameters

---
+++ estudy-roadmap/010functions/02functionParameters/index.js
@@ -0,0 +1,13 @@
+function toUpper(text){
+    const upperCased = text.toUpperCase();
+    console.log(upperCased);
+}
+
+toUpper('guilherme');
+
+const toLower = (text) => {
+    const lowerCased = text.toLowerCase();
+    console.log(lowerCased);
+}
+
+toLower('GUILHERME');
