83b3a728f8b26e5287b256e14d34c7430f9eee91
Guilherme Batista de Souza <146041046+guilhermebatista7@users.noreply.github.com>
2024-05-20 21:39:15 -0300

    Using this keyword in a method

---
+++ estudy-roadmap/010functions/07this/01method/index.js
@@ -0,0 +1,10 @@
+const person = {
+    firstName: "Guilherme",
+    age: 18,
+    allInformation: function () {
+        return this.firstName + ', ' + this.age; 
+    }
+};
+
+let res = person.allInformation();
+console.log(res);
